bfb819cf736ff2db4e6b1b80ea22db34a65da59e
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2020-03-11 12:30:57 +0200

    remove common folter

--- comon/components/AddEntry.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import { View } from "react-native";
-import { getMetricMetaInfo } from "../../src/utils/helpers";
-
-export default class AddEntry extends Component {
-  render() {
-    return <View>{getMetricMetaInfo("bike").getIcon}</View>;
-  }
-}
